4aa4ac94e1ad6362c2f86ee9f77c809a7d671656
Valerij Kalabin <valerijkalabin@yandex.ru>
2021-02-16 00:03:32 +0700

    feature: create about project component

---
+++ src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import './Main.css';
+import AboutProject from '../AboutProject/AboutProject'
+
+function Main() {
+  return (
+    <div className="main">
+      <AboutProject />
+    </div>
+  );
+}
+
+export default Main;
