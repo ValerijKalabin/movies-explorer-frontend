cd922e545eedaa05a2438e253baa0c7a86cde4b0
Valerij Kalabin <valerijkalabin@yandex.ru>
2021-02-16 16:34:45 +0700

    feature: create promo component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,11 @@
 import './Main.css';
+import Promo from '../Promo/Promo'
 import AboutProject from '../AboutProject/AboutProject'
 
 function Main() {
   return (
     <main className="main">
+      <Promo />
       <AboutProject />
     </main>
   );
---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,15 @@
+import './Promo.css';
+import landingLogo from '../../images/landing-logo.svg';
+
+function Promo() {
+  return (
+    <section className="promo">
+        <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
+        <div className="promo__container">
+          <img className="promo__logo" src={landingLogo} alt="Логотип Промо" />
+        </div>
+    </section>
+  );
+}
+
+export default Promo;
