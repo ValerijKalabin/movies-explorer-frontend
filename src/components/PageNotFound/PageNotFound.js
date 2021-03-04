55dcc981b884203a0f6771413bc8dd37d28c76ae
Valerij Kalabin <valerijkalabin@yandex.ru>
2021-03-04 21:55:58 +0700

    feature: create go back function

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,13 +1,26 @@
+import { useHistory } from 'react-router-dom';
 import './PageNotFound.css';
 
 function PageNotFound() {
+  const history = useHistory();
+
+  function handleClickButton() {
+    history.goBack();
+  }
+  
   return (
     <div className="false">
       <div className="false__container">
         <h1 className="false__title">404</h1>
         <p className="false__subtitle">Страница не найдена</p>
       </div>
-      <button className="false__button" type='button'>Назад</button>
+      <button
+        className="false__button"
+        type='button'
+        onClick={handleClickButton}
+      >
+        Назад
+      </button>
     </div>
   );
 }
